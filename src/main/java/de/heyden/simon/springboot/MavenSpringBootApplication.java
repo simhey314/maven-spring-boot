c51d07631e5357e8eb6a09b66d03a6c9c102f262
Simon Heyden <simon@family-heyden.net>
2020-11-09 21:40:51 +0100

    spring boot init

---
+++ src/main/java/de/heyden/simon/springboot/MavenSpringBootApplication.java
@@ -0,0 +1,13 @@
+package de.heyden.simon.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/heyden/simon/springboot/MavenSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package de.heyden.simon.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
