c4823aa9ceb8d18e7c4d91729272e8c44f9d9cec
Simon Heyden <simon@family-heyden.net>
2020-11-09 22:10:02 +0100

    running it tests

--- src/main/java/de/heyden/simon/springboot/controller/ExampleController.java
+++ src/main/java/de/heyden/simon/springboot/controller/ExampleController.java
@@ -1,5 +1,6 @@
 package de.heyden.simon.springboot.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -8,6 +9,7 @@ import java.util.Map;
 @RestController
 public class ExampleController {
 
+	@RequestMapping("/")
 	public Map<String, Object> getIndex(){
 		Map<String, Object> result = new HashMap<>();
 		result.put("hello","World!!!");
